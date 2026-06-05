3b3381157aa62af3c09221c332831c4f80b613d8
Your Name <you@example.com>
2026-06-05 19:56:53 +0300

    yaaa

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,3 @@
-import readlineSync from 'readline-sync';
-
-const ROUNDS_COUNT = 3;
-
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const isPrime = (num) => {
@@ -14,24 +10,9 @@ const isPrime = (num) => {
   return true;
 };
 
-export default function runGame(userName) {
-  console.log('\nGame: Prime Number');
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".\n');
-
-  for (let round = 0; round < ROUNDS_COUNT; round += 1) {
-    const number = getRandomNumber(1, 100);
-    const correctAnswer = isPrime(number) ? 'yes' : 'no';
-
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!\n`);
-      return;
-    }
-    console.log('Correct!\n');
-  }
-
-  console.log(`Congratulations, ${userName}! You won the game!\n`);
+export default function getGameData() {
+  const number = getRandomNumber(1, 100);
+  const question = String(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return { question, correctAnswer };
 }
